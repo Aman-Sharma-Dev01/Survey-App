38c56c03c2ff1845cb75e11d7a440fdaff50b629
Shivam Kumar Yadav <jeemainsdtu@gmail.com>
2025-11-16 17:55:52 +0530

    font-family changed
    font-family changed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,21 @@
+// Make sure to import the default theme if you're extending
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // Make sure this matches your project
   ],
   theme: {
-    extend: {},
+    extend: {
+      // Add your font family here
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+        // You could also add it as a custom name, e.g.:
+        // 'roboto': ['Roboto', 'sans-serif'],
+      },
+    },
   },
- plugins: [import('@tailwindcss/typography')],
+  plugins: [],
 }
